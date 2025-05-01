e2cbc8ea021643a2c9c7f058a666312ebc064205
Wonhee Jang <Qtsky89@gmail.com>
2025-05-01 13:56:09 +0900

    15. Onwards to a Better Project done

--- remix-course/app/routes/_index.tsx
+++ remix-course/app/routes/_index.tsx
@@ -11,8 +11,13 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <>
-      <h1>hello world!</h1>
-      <Link to="/demo">demo</Link>
+      <main id="content">
+        <h1>A better way of keeping track of your notes</h1>
+        <p>Try our early beta and never loose track of yourr notes again</p>
+        <p id="cta">
+          <Link to="/notes">Try Now!</Link>
+        </p>
+      </main>
     </>
   );
 }
