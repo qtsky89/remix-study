cd47fc5bee5dcaebfe2269e1335b3c1f2592f2ca
Wonhee Jang <Qtsky89@gmail.com>
2025-05-05 20:18:00 +0900

    41. Using Custom Fonts

--- remix-course-app/app/root.tsx
+++ remix-course-app/app/root.tsx
@@ -21,6 +21,16 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <Outlet />
