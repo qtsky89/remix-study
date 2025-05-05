202ef9a184f2a5beea4b082cbb9ff1b0a0369b07
Wonhee Jang <Qtsky89@gmail.com>
2025-05-05 20:15:22 +0900

    40. Adding Components & styling

--- remix-course-app/app/routes/auth.tsx
+++ remix-course-app/app/routes/auth.tsx
@@ -1,3 +1,9 @@
+import authStyles from "~/styles/auth.css";
+
 export default function View() {
   return <h1>auth</h1>;
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: authStyles }];
 }
