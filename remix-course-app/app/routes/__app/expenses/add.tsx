22f2fb6a8bd6c4a844387e3ae4e6ee193a18eecb
Wonhee Jang <Qtsky89@gmail.com>
2025-05-16 22:26:56 +0900

    Removed custom prisma output

--- remix-course-app/app/routes/__app/expenses/add.tsx
+++ remix-course-app/app/routes/__app/expenses/add.tsx
@@ -1,6 +1,8 @@
+import { redirect } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { addExpense } from "~/data/expenses.server";
 
 export default function View() {
   const navigate = useNavigate();
@@ -11,4 +13,11 @@ export default function View() {
   );
 }
 
-export function action() {}
+export async function action({ request }) {
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+  console.log(formData, expenseData);
+
+  await addExpense(expenseData);
+  return redirect("/expenses");
+}
