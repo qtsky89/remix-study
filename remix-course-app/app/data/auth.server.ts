c4def577385d8c9371a345a152e8af21e6e0e5ee
Wonhee Jang <Qtsky89@gmail.com>
2025-06-01 18:10:10 +0900

    94. Getting Started with User login

--- remix-course-app/app/data/auth.server.ts
+++ remix-course-app/app/data/auth.server.ts
@@ -1,8 +1,7 @@
 import { prisma } from "./database.server";
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 
 export async function signup({email, password}) {
-  
   const existingUser = await prisma.user.findFirst({where: {email}})
 
   if (existingUser) {
@@ -14,5 +13,24 @@ export async function signup({email, password}) {
   const passwordHash = await hash(password, 12)
 
   await prisma.user.create({data: {email: email, password: passwordHash}})
+
+}
+
+export async function login({email, password}) {
+  const existingUser = await prisma.user.findFirst({where: {email}})
+
+  if (!existingUser) {
+    const error = new Error('User Email is not exist')
+    error.status = 401
+    throw error
+  }
+
+  const passwordCorrect = await compare(password, existingUser.password)
+
+  if (!passwordCorrect) {
+    const error = new Error('Password is not correct')
+    error.status = 401
+    throw error
+  }
 
 }
