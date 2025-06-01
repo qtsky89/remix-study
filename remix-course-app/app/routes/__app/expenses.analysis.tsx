d9c2f1a792bb5a56dea821d2e5bcf9ff660b8378
Wonhee Jang <Qtsky89@gmail.com>
2025-06-01 14:36:59 +0900

    84. Practice Time - Solution

--- remix-course-app/app/routes/__app/expenses.analysis.tsx
+++ remix-course-app/app/routes/__app/expenses.analysis.tsx
@@ -1,26 +1,63 @@
 import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
 import Chart from "~/components/expenses/Chart";
+import { getExpenses } from "~/data/expenses.server";
+import { useCatch, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import Error from "~/components/util/Error";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "id1",
-    title: "First Expense",
-    amount: 12.99,
-    date: new Date().toISOString(),
-  },
-  {
-    id: "id2",
-    title: "Second Expense",
-    amount: 6.99,
-    date: new Date().toISOString(),
-  },
-];
+// const DUMMY_EXPENSES = [
+//   {
+//     id: "id1",
+//     title: "First Expense",
+//     amount: 12.99,
+//     date: new Date().toISOString(),
+//   },
+//   {
+//     id: "id2",
+//     title: "Second Expense",
+//     amount: 6.99,
+//     date: new Date().toISOString(),
+//   },
+// ];
 
 export default function View() {
+  const expenses = useLoaderData();
+
   return (
     <main>
-      <Chart expenses={DUMMY_EXPENSES} />
-      <ExpenseStatistics expenses={DUMMY_EXPENSES} />
+      <Chart expenses={expenses} />
+      <ExpenseStatistics expenses={expenses} />
+    </main>
+  );
+}
+
+export async function loader() {
+  const expenses = await getExpenses();
+
+  if (!expenses || expenses.length === 0) {
+    throw json(
+      { message: "Couldnt load expenses for the request analysis." },
+      {
+        status: 404,
+        statusText: "Expenses not found",
+      }
+    );
+  }
+
+  return expenses;
+}
+
+export function CatchBoundary() {
+  const caughtResponse = useCatch();
+
+  return (
+    <main>
+      <Error title={caughtResponse.statusText}>
+        <p>
+          {caughtResponse.data?.message ||
+            "Something went wrong. Coulnd't fetch expenses"}
+        </p>
+      </Error>
     </main>
   );
 }
